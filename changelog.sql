66a0d123e3d65d81f0a958fdd2fd8651e8e7e53a
josembar <jose.barrantes@hotmail.es>
2024-07-11 22:41:26 -0500

    feat: include more changesets

--- changelog.sql
+++ changelog.sql
@@ -4,3 +4,15 @@
 CREATE TABLE test_table (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id))
 --rollback DROP TABLE test_table
 
+--changeset jose.barrantes:2
+INSERT INTO test_table VALUES (1, 1000)
+--rollback DELETE FROM test_table
+
+--changeset jose.barrantes:3
+INSERT INTO test_table VALUES (10, 10000)
+--rollback DELETE FROM test_table WHERE test_id = 10
+
+--changeset jose.barrantes:4
+UPDATE test_table
+SET test_column = 20000
+WHERE test_id = 10 
