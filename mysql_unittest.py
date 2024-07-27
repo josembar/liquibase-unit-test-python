ef4175a6410a2e818d1936cf0f60c802b0094109
josembar <jose.barrantes@hotmail.es>
2024-07-27 13:22:06 -0500

    feat: unit tests

---
+++ mysql_unittest.py
@@ -0,0 +1,42 @@
+import unittest
+from sqlalchemy import create_engine, text
+
+class TestMySQLDBMethods(unittest.TestCase):
+    
+    @staticmethod
+    def run_query(query:str):
+        # Connect to the database
+        engine = create_engine("mysql+mysqlconnector://jose:jose_demo@172.17.0.2/demo")
+        connection = engine.connect()
+        # Run the query
+        result = connection.execute(text(query))
+        # Get a list of tuples as result
+        result = result.all()
+        #print("result {0}".format(result))
+        # Close connection
+        connection.close()
+        engine.dispose()
+        # Return query result
+        return result
+
+    def test_connection(self):
+        result = self.run_query("SELECT 1")
+        result = result[0][0]
+        self.assertEqual(result, 1)
+    
+    def test_check_if_table_exist(self):
+        query = """
+            SELECT EXISTS (
+                SELECT TABLE_NAME
+                FROM information_schema.TABLES 
+                WHERE TABLE_SCHEMA = (SELECT DATABASE())
+                AND TABLE_TYPE = 'BASE TABLE'
+                AND TABLE_NAME = 'DATABASECHANGELOG'
+            ) AS DO_EXIST;
+        """
+        result = self.run_query(query)
+        result = result[0][0]
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
