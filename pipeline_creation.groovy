4dbb6f58ebb51eea75199ebe776750f73af06d3c
josembar <jose.barrantes@hotmail.es>
2024-07-30 20:50:44 -0500

    feat: new script to create pipelines after jenkins initialization

---
+++ pipeline_creation.groovy
@@ -0,0 +1,15 @@
+import hudson.plugins.git.*;
+
+def scm = new GitSCM("https://github.com/josembar/liquibase-unit-test-python.git")
+scm.branches = [new BranchSpec("*/main")];
+
+def pipelineDefinition = ["liquibase-run-update": "Jenkinsfile-Update", "liquibase-run-rollback": "Jenkinsfile-Rollback", "database-unit-test": "Jenkinsfile-UnitTest"]
+def flowDefinition, parent, job
+
+for (element in pipelineDefinition) {
+  	flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, element.value)
+	parent = Jenkins.instance
+	job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, element.key)
+	job.definition = flowDefinition
+	parent.reload()
+}
