8ab4378a67f8659b15a5d0af90d5e2d2510e1193
josembar <jose.barrantes@hotmail.es>
2024-08-26 15:15:41 -0500

    refactor: script to check status

---
+++ liquibase_check_status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+liquibase status \
+    --classpath=$DRIVER_PATH_LIQUIBASE \
+    --url=jdbc:mysql://$DEMO_DB_HOST:$DEMO_DB_PORT/$DEMO_DB_NAME \
+    --changeLogFile=changelog.sql \
+    --username=$MYSQL_USER \
+    --password=$MYSQL_PASSWORD
