293003dfdf04db6f16697eddb5a5ff39bc09cd43
josembar <jose.barrantes@hotmail.es>
2024-08-27 15:19:32 -0500

    refactor: moved all jenkinsfiles

--- jenkins/init_scripts/pipeline_creation.groovy
+++ jenkins/init_scripts/pipeline_creation.groovy
@@ -6,11 +6,16 @@ import hudson.model.*
 def scm = new GitSCM("https://github.com/josembar/liquibase-unit-test-python.git")
 scm.branches = [new BranchSpec("*/main")];
 
-def pipelineDefinition = ["liquibase-run-update": "Jenkinsfile-Update", "liquibase-run-rollback": "Jenkinsfile-Rollback", "database-unit-test": "Jenkinsfile-UnitTest"]
+def pipelineDefinition = [
+	"liquibase-run-update": "Jenkinsfile-Update",
+	"liquibase-run-rollback": "Jenkinsfile-Rollback",
+	"database-unit-test": "Jenkinsfile-UnitTest"
+]
+def dir = "jenkins/pipeline_jobs/"
 def flowDefinition, parent, job
 
 for (element in pipelineDefinition) {
-  	flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, element.value)
+  	flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "${dir}${element.value}")
 	parent = Jenkins.instance
 	job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, element.key)
 	job.definition = flowDefinition
